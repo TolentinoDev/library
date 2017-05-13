9fcdd38fdfcff8474b8586f2a94f913bb18f15a8
Ryan Tolentino <Tolentino2016@gmail.com>
2017-05-12 20:07:31 -0400

    added images
    added images

--- libraryalgorithm.py
+++ libraryalgorithm.py
@@ -8,18 +8,20 @@
 
 #!/usr/bin/python
 
-if description contrains[x] terms:
-	insert(y) 
-with open('output.csv','a') as e:
+#if description contrains[x] terms:
+	#insert(y) 
+with open('output1.csv','a') as e:
 	with open('db.csv','r')as f:
 		for line in f:
 			tokens = line.split("|")
 			description = tokens[2]
-			with open ('subjest.csv','r')as g:
-				for line in g:
-					if line in description:
-						new=tokens[0]+"1"+line+tokens[2]
-						e.write(new+"\n")
-					else:
-						e.write<"|".join(tokens>)
+			subject = tokens[1]
+			if len(subject) == 0:
+                                with open ('subject.csv','r')as g:
+                                        for line in g:
+                                                if line in description:
+                                                        new=tokens[0]+"1"+line+tokens[2]
+                                                        e.write(new+"\n")
+                                                else:
+                                                        e.write("|".join(tokens))
 
